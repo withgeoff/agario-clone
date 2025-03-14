6f93adcf3ed2a110628c521f36cc45bf43c9fa49
Geoff Cox <geoff@withgeoff.com>
2025-03-14 00:41:07 -0400

    Let's get this server started!

---
+++ server/src/main/java/net/geoffcox/agario_clone/AgarioCloneApplication.java
@@ -0,0 +1,13 @@
+package net.geoffcox.agario_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgarioCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgarioCloneApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/net/geoffcox/agario_clone/AgarioCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package net.geoffcox.agario_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgarioCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
