0c37776277ba34216ebaf6d787865d6c1f359b0a
Geoff Cox <geoff@withgeoff.com>
2025-03-14 01:36:54 -0400

    Set up dev enviroment with PostgreSQL. Let's drop Liquibase for the moment. Simplify.

--- server/src/main/java/net/geoffcox/agarioclone/AgarioCloneApplication.java
+++ server/src/main/java/net/geoffcox/agarioclone/AgarioCloneApplication.java
@@ -1,4 +1,4 @@
-package net.geoffcox.agario_clone;
+package net.geoffcox.agarioclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
